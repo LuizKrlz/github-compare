be107b57bfa85d097e2d43f1f2848d6e82eeabca
Luiz Carlos <luiz.correa.dev@gmail.com>
2019-02-12 19:36:28 -0200

    cath error and stylization input case has error

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -8,6 +8,7 @@ import CompareList from '../../components/CompareList';
 
 export default class Main extends Component {
   state = {
+    repositoryError: false,
     repositoryInput: '',
     repositories: [],
   };
@@ -23,20 +24,21 @@ export default class Main extends Component {
       this.setState({
         repositoryInput: '',
         repositories: [...repositories, repository],
+        repositoryError: false,
       });
     } catch (error) {
-      console.log(error);
+      this.setState({ repositoryError: true });
     }
   };
 
   render() {
-    const { repositories, repositoryInput } = this.state;
+    const { repositoryError, repositories, repositoryInput } = this.state;
 
     return (
       <Container>
         <img src={logo} alt="Github Compare" />
 
-        <Form onSubmit={this.handleAddRepository}>
+        <Form withError={repositoryError} onSubmit={this.handleAddRepository}>
           <input
             type="text"
             placeholder="usuário/repositório"
