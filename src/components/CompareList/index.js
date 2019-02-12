4b2fe2fc9ac31ca70ebd5e803231dff8320e8a92
luiz.correa <luiz.correa@bludata.com.br>
2019-02-12 12:41:51 -0200

    list of repositories

---
+++ src/components/CompareList/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const CompareList = () => (
+  <div>
+    <div>
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="Facebook" />
+        <strong>React</strong>
+        <small>facebook</small>
+      </header>
+      <ul>
+        <li>
+          95,0190
+          <small> stars</small>
+        </li>
+        <li>
+          95,0190
+          <small> forks</small>
+        </li>
+        <li>
+          95,0190
+          <small> issues</small>
+        </li>
+        <li>
+          3 days ago
+          <small> las commit</small>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default CompareList;
